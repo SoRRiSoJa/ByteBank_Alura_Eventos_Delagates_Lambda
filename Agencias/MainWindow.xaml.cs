a7b38abd41c43ea28b1e52fc79c3f6b8c40a02b5
João André Martins Dias e Silva <joao.silva@marttech.com.br>
2020-12-31 10:10:59 -0300

    [Fim módulo 2 Delegates] - Implementado novo formulario WPF, Assinado novos eventos para combinação de funcionalidade nos métodos de ação dos botões. Desabilitado componente AgenciaListBox e implementado listbox padrão.

--- Agencias/MainWindow.xaml.cs
+++ Agencias/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Agencias.Components;
 using Agencias.Data;
+using Agencias.Model;
+using Agencias.View;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,22 +14,28 @@ namespace Agencias
     public partial class MainWindow : Window
     {
         private readonly Context _context = new Context();
-        private readonly AgenciasListBox lstAgencias;
+        private readonly ListBox lstAgencias;
         public MainWindow()
         {
-            this.lstAgencias = new AgenciasListBox(this);
+            this.lstAgencias = new ListBox();
             InitializeComponent();
             AtualizarControles();
         }
 
         private void AtualizarControles()
         {
+            lstAgencias.SelectionChanged += new SelectionChangedEventHandler(lstAgencias_SelectionChanged);
             lstAgencias.Width = 380;
             lstAgencias.Height = 380;
-            Canvas.SetTop (lstAgencias, 15);
-            Canvas.SetLeft (lstAgencias, 15);
+            Canvas.SetTop(lstAgencias, 15);
+            Canvas.SetLeft(lstAgencias, 15);
 
             container.Children.Add(lstAgencias);
+            LoadAndUpdateList();
+        }
+
+        private void LoadAndUpdateList()
+        {
             lstAgencias.Items.Clear();
             var agencias = _context.Agencia.ToList();
             foreach (var agencia in agencias)
@@ -36,6 +44,15 @@ namespace Agencias
             }
         }
 
+        private void lstAgencias_SelectionChanged(object sender, RoutedEventArgs e) 
+        {
+            var agenciaSelecionada = lstAgencias.SelectedItem as Agencia;
+            txtNumero.Text = agenciaSelecionada.Numero;
+            txtNome.Text = agenciaSelecionada.Nome;
+            txtDescricao.Text = agenciaSelecionada.Descricao;
+            txtTelefone.Text = agenciaSelecionada.Telefone;
+            txtEndereco.Text = agenciaSelecionada.Endereco;
+        }
         private void btnExcluir_Click(object sender, RoutedEventArgs e)
         {
             var confirmacao=MessageBox.Show("Você deseja realmente excluir essa buceta?","Confirmação",MessageBoxButton.YesNo);
@@ -47,6 +64,29 @@ namespace Agencias
             { 
             
             }
+        }
+
+        private void btnEditar_Click(object sender, RoutedEventArgs e)
+        {
+            Agencia agenciaSelecionada = lstAgencias.SelectedItem as Agencia;
+            if (!(agenciaSelecionada is null))
+            {
+                var wpfForm = new EdicaoAgencias(agenciaSelecionada);
+                var result=wpfForm.ShowDialog().Value;
+                if (result)
+                {
+                    //Clicou em salvar
+                }
+                else 
+                { 
+                    //Clicou cnacelar
+                }
+            }
+        }
+
+        private void btnEditar_Click_1(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ Agencias/View/EdicaoAgencias.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Agencias.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Agencias.View
+{
+    /// <summary>
+    /// Lógica interna para EdicaoAgencias.xaml
+    /// </summary>
+    public partial class EdicaoAgencias : Window
+    {
+        private readonly Agencia _agencia;
+        public EdicaoAgencias(Agencia agencia)
+        {
+            InitializeComponent();
+            this._agencia = agencia?? throw new ArgumentNullException(nameof(agencia));
+            UpdateTextFields();
+            UpdateControls();
+
+        }
+        private void UpdateTextFields() 
+        {
+            txtNumero.Text = _agencia.Numero;
+            txtNome.Text = _agencia.Nome;
+            txtDescricao.Text = _agencia.Descricao;
+            txtTelefone.Text = _agencia.Telefone;
+            txtEndereco.Text = _agencia.Endereco;
+        }
+        private void UpdateControls() 
+        {
+            //var salvarkEventHandler = (RoutedEventHandler) btnSalvar_Click + CloseWindow;
+            //var cancelarEventHandler = (RoutedEventHandler) btnCalcelar_Click + CloseWindow;
+
+            btnSalvar.Click += new RoutedEventHandler(CloseWindow);
+            btnCalcelar.Click += new RoutedEventHandler(CloseWindow);
+        }
+        private void btnSalvar_Click(object sender, RoutedEventArgs e)=>
+            DialogResult = true;
+        
+
+        private void btnCalcelar_Click(object sender, RoutedEventArgs e)=>
+            DialogResult = false;
+        
+        private void CloseWindow(object sender, RoutedEventArgs e) =>
+            Close();
+        
+    }
+}
